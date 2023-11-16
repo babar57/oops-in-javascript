756a0168bdab957583bf7f4cedf676a97da1c39d
babar57 <147683500+babar57@users.noreply.github.com>
2023-11-16 11:03:08 +0500

    oop

---
+++ seassion.html/oops.js
@@ -0,0 +1,23 @@
+// // spread array
+// let arr1 = [1,2,3,4];
+// let arr2 = ["talha","abid","ali","basit"]
+// let mergedArray =[...array1, ...array2,...otherinfo]
+// console.log(mergedArray)
+
+
+class person{
+
+    constructor(name, age, country){
+        this.name = name;
+        this.age = age;
+        this.country = country;
+        details() (console.log(this.name, this.age,this.country))
+
+
+    }
+}
+let person1 = new person("syedtalha", 22, "pakistan")
+let person2 = new person("babar", 25, "pakistan")
+
+console.log(person1.name)
+
